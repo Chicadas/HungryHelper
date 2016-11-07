e1efa7655ed594c4e9543d5372b7bcade6698f99
Shane Jackson <[sjacksondev@gmail.com]>
2016-11-06 22:27:29 -0800

    creating database class

--- app/src/main/java/project/comp3717/hungryhelper/DisplayRecipeActivity.java
+++ app/src/main/java/project/comp3717/hungryhelper/DisplayRecipeActivity.java
@@ -16,13 +16,5 @@ public class DisplayRecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_recipe);
 
-        Bundle b = this.getIntent().getExtras();
-        String[] words = b.getStringArray("key");
-        String everything = "";
-        TextView text = (TextView)findViewById(R.id.textView6);
-        for(int i = 0; i < words.length; i++){
-            everything += words[i] + ", ";
-        }
-        text.setText(everything);
     }
 }
---
+++ app/src/main/java/project/comp3717/hungryhelper/MyDBHandler.java
@@ -0,0 +1,7 @@
+package project.comp3717.hungryhelper;
+
+/**
+ * Created by shanejackson on 2016-11-06.
+ */
+public class MyDBHandler {
+}
