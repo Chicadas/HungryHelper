f9a47afb688e01bb1348235dd7c944339ce18d66
Shane Jackson <[sjacksondev@gmail.com]>
2016-11-07 14:37:40 -0800

    database cont

--- app/src/main/java/project/comp3717/hungryhelper/DisplayRecipeActivity.java
+++ app/src/main/java/project/comp3717/hungryhelper/DisplayRecipeActivity.java
@@ -1,5 +1,8 @@
 package project.comp3717.hungryhelper;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,19 +13,48 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class DisplayRecipeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_recipe);
+        String recipeName = "Lasagna";
         setTitle("Recipe Name");
-
-        ImageView imageView = (ImageView)findViewById(R.id.recipeImage);
+        /**
+         * Handling ingredients
+         */
+        MyDBHandler myDBingredients = new MyDBHandler(this, null, null, 1);
+        ArrayList<String> ingredients =  myDBingredients.displayIngredients(recipeName);
         TextView ingredientText = (TextView)findViewById(R.id.ingredients);
+        String val = ingredients.get(0);
+        ingredientText.setText(val);
+        myDBingredients.close();
+        /**
+         * Handling instructions
+         */
+        MyDBHandler myDBinstructions = new MyDBHandler(this, null, null, 1);
+        ArrayList<String> instructions =  myDBinstructions.displayInstructions(recipeName);
         TextView instructionText = (TextView)findViewById(R.id.instructions);
+        String val2 = instructions.get(0);
+        instructionText.setText(val2);
+        myDBinstructions.close();
+        /**
+         * Handling images
+         */
+        // android:src ="@drawable/lasanga"
+        MyDBHandler myDBimages = new MyDBHandler(this, null, null, 1);
+        ArrayList<Integer> images =  myDBimages.displayImage(recipeName);
+        ImageView imageView = (ImageView)findViewById(R.id.recipeImage);
+        int val3 = images.get(0);
+        imageView.setImageResource(val3);
+        myDBimages.close();
+
 
 
     }
+
 
 }
--- app/src/main/java/project/comp3717/hungryhelper/MyDBHandler.java
+++ app/src/main/java/project/comp3717/hungryhelper/MyDBHandler.java
@@ -4,15 +4,21 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by shanejackson on 2016-11-06.
  */
 public class MyDBHandler extends SQLiteOpenHelper {
-
+    // DatabseUtils.dumbCurstontoString(curstor
+    // int numb = cursor.getCount
+    // get column names
+    // google cusrsor get column names
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "HungryHelper.db";
+    private static final String DATABASE_NAME = "Hungry.db";
     private static final String TABLE_RECIPES = "Recipes";
 
     public static final String COLUMN_ID = "_id";
@@ -33,15 +39,11 @@ public class MyDBHandler extends SQLiteOpenHelper {
                 + COLUMN_RECIPE_NAME + " TEXT NOT NULL, "
                 + COLUMN_RECIPE_INGREDIENTS + "  TEXT NOT NULL, "
                 + COLUMN_RECIPE_INSTRUCTIONS + " TEXT NOT NULL, "
-                + COLUMN_RECIPE_IMAGE + " BLOB EXT NOT NULL"
+                + COLUMN_RECIPE_IMAGE + " INTEGER "
                 + ")";
         db.execSQL(CREATE_PRODUCTS_TABLE);
 
-        db.execSQL("INSERT INTO " + TABLE_RECIPES + "(NAME, INGREDIENTS, INTSTRUCTIONS, IMAGE) VALUES ('Lasagna', 'Beef and Noodles what else', 'In a Dutch oven, cook sausage, ground beef, onion, and garlic over medium heat until well browned. Stir in crushed tomatoes, tomato paste, tomato sauce, and water. Season with sugar, basil, fennel seeds, Italian seasoning, 1 tablespoon salt, pepper, and 2 tablespoons parsley. Simmer, covered, for about 1 1/2 hours, stirring occasionally.\n" +
-                "Bring a large pot of lightly salted water to a boil. Cook lasagna noodles in boiling water for 8 to 10 minutes. Drain noodles, and rinse with cold water. In a mixing bowl, combine ricotta cheese with egg, remaining parsley, and 1/2 teaspoon salt.\n" +
-                "Preheat oven to 375 degrees F (190 degrees C).\n" +
-                "To assemble, spread 1 1/2 cups of meat sauce in the bottom of a 9x13 inch baking dish. Arrange 6 noodles lengthwise over meat sauce. Spread with one half of the ricotta cheese mixture. Top with a third of mozzarella cheese slices. Spoon 1 1/2 cups meat sauce over mozzarella, and sprinkle with 1/4 cup Parmesan cheese. Repeat layers, and top with remaining mozzarella and Parmesan cheese. Cover with foil: to prevent sticking, either spray foil with cooking spray, or make sure the foil does not touch the cheese.\n" +
-                "Bake in preheated oven for 25 minutes. Remove foil, and bake an additional 25 minutes. Cool for 15 minutes before serving.', ''img )");
+        db.execSQL("INSERT INTO " + TABLE_RECIPES + "(NAME, INGREDIENTS, INTSTRUCTIONS, IMAGE) VALUES ('Lasagna', 'Beef and Noodles what else', 'Make it bitch'," + R.drawable.lasanga + ")");
     }
     @Override
     public void onUpgrade (SQLiteDatabase db,int oldVersion,
@@ -49,4 +51,77 @@ public class MyDBHandler extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_RECIPES);
         onCreate(db);
     }
+
+    /**
+     * Returns an ingredient list for a given recipe.
+     * @param recipeName
+     * @return An arrarylist of  ingredients
+     */
+    public ArrayList<String> displayIngredients(String recipeName){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String sqlStatement = "SELECT * FROM " + TABLE_RECIPES + " WHERE " + COLUMN_RECIPE_NAME + "=\'"
+                + recipeName + "\'";
+        Cursor c = db.rawQuery(sqlStatement, null);
+
+        ArrayList<String> list = new ArrayList<String>();
+        c.moveToFirst();
+        while(c.isAfterLast() == false) {
+            list.add(c.getString(2));
+            Log.d("TRYING TO CREATE", c.getString(2));
+            c.moveToNext();
+        }
+        c.close();
+        db.close();
+        return list;
+    }
+    /**
+     * Returns an instruction set for a given recipe.
+     * @param recipeName
+     * @return An arrarylist of instructions
+     */
+    public ArrayList<String> displayInstructions(String recipeName){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String sqlStatement = "SELECT * FROM " + TABLE_RECIPES + " WHERE " + COLUMN_RECIPE_NAME + "=\'"
+                + recipeName + "\'";
+        Cursor c = db.rawQuery(sqlStatement, null);
+
+        ArrayList<String> list = new ArrayList<String>();
+        c.moveToFirst();
+        while(c.isAfterLast() == false) {
+            list.add(c.getString(3));
+            Log.d("TRYING TO CREATE", c.getString(2));
+            c.moveToNext();
+        }
+        c.close();
+        db.close();
+        return list;
+    }
+    /**
+     * Returns an image for a given recipe.
+     * @param recipeName
+     * @return An arrarylist of images
+     */
+    public ArrayList<Integer> displayImage(String recipeName) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String sqlStatement = "SELECT * FROM " + TABLE_RECIPES + " WHERE " + COLUMN_RECIPE_NAME + "=\'"
+                + recipeName + "\'";
+
+        Cursor c = db.rawQuery(sqlStatement, null);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        c.moveToFirst();
+
+        String names[] = c.getColumnNames();
+        for(String s: names){
+            Log.d("column names ", s);
+        }
+        while(c.isAfterLast() == false) {
+            Log.d("TRYING TO CREATE", c.getString(4));
+            list.add(c.getInt(4));
+            c.moveToNext();
+        }
+        c.close();
+        db.close();
+        return list;
+    }
+
 }
