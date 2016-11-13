298a3dadbf456590c7294c070f5e930c042c38e7
Shane Jackson <[sjacksondev@gmail.com]>
2016-11-13 13:49:18 -0800

    database cont

--- app/src/main/java/project/comp3717/hungryhelper/MatchesActivity.java
+++ app/src/main/java/project/comp3717/hungryhelper/MatchesActivity.java
@@ -1,8 +1,11 @@
 package project.comp3717.hungryhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MatchesActivity extends AppCompatActivity {
@@ -14,11 +17,21 @@ public class MatchesActivity extends AppCompatActivity {
 
         Bundle b = this.getIntent().getExtras();
         String[] words = b.getStringArray("key");
-
         TextView text = (TextView)findViewById(R.id.textView6);
         for(int i = 0; i < words.length; i++){
             text.setText(words[i]);
             Log.d("From spinner: ",words[i]);
         }
+    }
+
+    protected void onListItemClick(ListView l, final View v, int position, long id) {
+
+        onListItemClick(l, v, position, id);
+        //int itemPosition     = position;
+        final String  itemValue    = (String) l.getItemAtPosition(position);
+
+        Intent intent = new Intent(this, DisplayRecipeActivity.class);
+        //intent.putExtra("courses",itemValue);
+        startActivity(intent);
     }
 }
